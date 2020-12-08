01ffae0d4febf1640d17454e29e33167fec5680a
Zihong Lin <harrylin317@gmail.com>
2020-12-08 22:59:07 +0800

    Add name for benchmark

--- bench_test.go
+++ bench_test.go
@@ -7,13 +7,11 @@ import (
 	"uk.ac.bris.cs/gameoflife/gol"
 )
 
-func BenchmarkParallel(b *testing.B) {
+func BenchmarkExecutionTime(b *testing.B) {
 	//os.Stdout = nil
 	testParams := gol.Params{ImageWidth: 512, ImageHeight: 512, Turns: 1000}
 	for i := 0; i < b.N; i++ {
-
 		for threads := 1; threads <= 16; threads++ {
-
 			testArgument := fmt.Sprint(threads)
 			b.Run(testArgument, func(b *testing.B) {
 				testParams.Threads = threads
